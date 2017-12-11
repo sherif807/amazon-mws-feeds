81259e530a1583851ad2db4a5e87e965592332b7
Sherif Badawy <s8077@hotmail.com>
2017-12-11 15:50:52 -0500

    changing ssl verify

--- src/MarketplaceWebService/Client.php
+++ src/MarketplaceWebService/Client.php
@@ -1052,7 +1052,7 @@ class MarketplaceWebService_Client implements MarketplaceWebService_Interface
       CURLOPT_VERBOSE => true,
       CURLOPT_HEADERFUNCTION => array ($this, 'headerCallback'),
       CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_SSL_VERIFYPEER => true,
+      CURLOPT_SSL_VERIFYPEER => false,
       CURLOPT_SSL_VERIFYHOST => 2
     );
   }
